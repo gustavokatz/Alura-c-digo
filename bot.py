6f76855bcd8080e371c83acdeb5846d2f4e30bc7
gustavokatz <62847942+gustavokatz@users.noreply.github.com>
2021-04-02 22:52:32 -0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
+# Para que o código funcione correntamente, favor instalar as bibliotecas 'beautifulsoup4' e 'google'.
+# Para as instalar, basta digitar pip install (biblioteca)
 
+
+# Adiquirição de informação
 from googlesearch import search
 print ('Olá, bem vindo ao Bot Vtex\n')
 nome = input('Como você gostaria de ser chamado?\n')
 residencia = input(f'Certo, {nome}, em que cidade você mora?\n')
 preparo = input(f'Muito bem {nome}, já decidiu o que vai querer pedir?\n')
-#def (start):
+
+# Criação da lista de compra
 while preparo.lower() == 'sim':
     lista2 = input('Quais itens gostaria de colocar no carrinho?\n')
     lista2 = lista2.split(',')
@@ -12,14 +17,15 @@ while preparo.lower() == 'sim':
     while preparo.lower() == 'sim':
         lista2 = input(f'Quais outros itens gostaria alem de {lista2}\n').split(',') + lista2 
         preparo = input('quer continuar sua compra?\n')
-    
 print(f'Muito bem, vou finalizar sua compra, sua lista é {lista2}')
 
+# Correção da lista para obter itens compraveis relacionados a Vtex e a residência
 i=0
 while i < len(lista2):
-    lista2[i]= 'comprar ' + lista2[i] + 'Vtex ' +  residencia
+    lista2[i]= 'comprar ' + lista2[i] + ' Vtex ' +  residencia
     i+=1
 
+# Pesquisa da lista no Google
 i=0
 while i < len(lista2):
     query = lista2[i]
